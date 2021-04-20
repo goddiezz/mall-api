5d6c51fb694798ace7b1bc0192c136b4ec2af93c
loorzve <goddiezz@gmail.com>
2021-04-20 10:40:25 +0800

    楼楼商城api项目初始化

---
+++ src/test/java/top/PotatoRice/mallapi/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.PotatoRice.mallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
